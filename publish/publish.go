30715573ea9ad7804c827c7ca63a090417112c3b
zhangkejie <kejie.zhang@hyperchain.cn>
2017-10-21 15:14:25 +0800

    feat:Add go-ipfs-api and func publish.

---
+++ publish/publish.go
@@ -0,0 +1,32 @@
+package publish
+
+import (
+	api"github.com/ipfs/go-ipfs-api"
+	"github.com/gogank/papillon/utils"
+	"strings"
+)
+
+type publish interface {
+	AddFile(filename string) (string, error)
+	AddDir(dir string) (string, error)
+}
+
+type PublishImpl struct {
+	shell *api.Shell
+}
+
+func NewPublishImpl(url string) *PublishImpl {
+	return &PublishImpl{
+		shell: api.NewShell(url),
+	}
+}
+
+func (publish *PublishImpl) AddFile(filename string) (string, error) {
+	contents,err := utils.ReadFile(filename)
+	if err != nil {
+		return "",err
+	}
+	reader := strings.NewReader(string(contents))
+	hash,err := publish.shell.Add(reader)
+	return hash,err
+}
