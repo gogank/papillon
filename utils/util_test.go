55c39188d2b373ef8a005f63afb223ff08fe0f31
zhangkejie <kejie.zhang@hyperchain.cn>
2017-10-21 14:29:10 +0800

    feat:Add util and finish cmd.

--- utils/util_test.go
+++ utils/util_test.go
@@ -14,3 +14,13 @@ func TestMkfile(t *testing.T) {
 	res := Mkfile("./test/test.html",[]byte("papillon"))
 	assert.Equal(t,true,res)
 }
+
+func TestExistDir(t *testing.T) {
+	res := ExistDir("./test")
+	assert.Equal(t,true,res)
+}
+
+func TestListDir(t *testing.T) {
+	file,_ := ListDir("./test","html")
+	assert.Equal(t,"test.html",file[0])
+}
