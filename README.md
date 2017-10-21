14eb392f7f6706bc5966b979a352919d36ef4598
zhangkejie <kejie.zhang@hyperchain.cn>
2017-10-21 11:52:02 +0800

    fix:delete binary files.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # papillon
 A project for GO HACKATHON 2017 by golangfoundation.org
+
+## Compile
--- cmd/generate.go
+++ cmd/generate.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"fmt"
+	"certgen/certUtils"
+	"github.com/gogank/papillon/utils"
 )
 
 func init() {
@@ -14,6 +17,20 @@ var generateCmd = &cobra.Command{
 	Short: "Genrate a static blog website.",
 	Long:  `Generate a private key pem , and save to a specific file path`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		if len(args) < 2{
+			fmt.Println("Please specific the private key path and the public key path")
+			fmt.Println("Example:")
+			fmt.Println("certgen gc ./test.priv ./test.pub")
+			return
+		}
+		fmt.Println("Generate a private key")
+		privPath := utils.Abs(args[0])
+		pubPath := utils.Abs(args[1])
+		fmt.Println("save the private key into ",privPath)
+		err := certUtils.GeneratePrivKeyFile(privPath,pubPath)
+		if err != nil {
+			fmt.Println("generate the priv file failed!")
+		}
+		fmt.Println("generate the priv file success")
 	},
 }
