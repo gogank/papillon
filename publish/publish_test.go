ca6068842244cd7da557f6c4181d5760f0d68728
zhangkejie <kejie.zhang@hyperchain.cn>
2017-10-21 16:17:53 +0800

    feat:add func file list.

--- publish/publish_test.go
+++ publish/publish_test.go
@@ -1,16 +1,41 @@
 package publish
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"fmt"
+)
 
 const (
 	shellUrl     = "localhost:5001"
 )
 
+var pub *PublishImpl
+
+func init()  {
+	pub = NewPublishImpl(shellUrl)
+}
+
 func TestPublishImpl_AddFile(t *testing.T) {
-	publish := NewPublishImpl(shellUrl)
-	hash,err := publish.AddFile("./test/gogank.jpg")
+	hash,err := pub.AddFile("./test/gogank.jpg")
 	if err != nil {
 		t.Error(err)
 	}
+	assert.Nil(t,err)
 	t.Log(hash)
+}
+
+func TestPublishImpl_AddDir(t *testing.T) {
+	hash,err := pub.AddDir("/Users/DeepSea/Documents/workspace/workspace_go/src/github.com/gogank/papillon/publish/test/.")
+	if err != nil {
+		t.Error(err)
+	}
+	assert.Nil(t,err)
+	t.Log(hash)
+}
+
+func TestPublishImpl_AddFile2(t *testing.T) {
+	objs,_ := pub.shell.FileList(fmt.Sprintf("/ipfs/%s", "QmXqBwdxJHZPS8he9LZYpx1APyh4Kx8jEHccnmuVKyseag"))
+	lists := objs.Links
+	t.Log(len(lists))
 }
