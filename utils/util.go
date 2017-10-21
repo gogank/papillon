945a7d8ad57d270f845fbc45b4ff39e73b47dba3
zhangkejie <kejie.zhang@hyperchain.cn>
2017-10-21 13:56:59 +0800

    feat:Add some file util.

--- utils/util.go
+++ utils/util.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 func Exist(filename string) bool {
@@ -51,4 +53,31 @@ func Mkfile(filename string,file []byte) bool {
 	}
 	f.Close();
 	return true
+}
+
+func ListDir(dirPth string, suffix string) (files []string, err error) {
+	files = make([]string, 0, 10)
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+	PthSep := string(os.PathSeparator)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			continue
+		}
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) { //匹配文件
+			files = append(files, dirPth+PthSep+fi.Name())
+		}
+	}
+	return files, nil
+}
+
+func ReadFile(filename string) ([]byte,error) {
+	contents,err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil,err
+	}
+	return contents,nil
 }
