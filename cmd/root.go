8f9d758e14cc3ecf850fe31cfdb10aa5ea3cc1f2
zhangkejie <kejie.zhang@hyperchain.cn>
2017-10-22 10:16:00 +0800

    feat:add papillon in rootcmd.

--- cmd/root.go
+++ cmd/root.go
@@ -9,9 +9,15 @@ var RootCmd = &cobra.Command{
 	Use:   "papi",
 	Short: "Papillon is a distribution static blog publish system.",
 	Long: `A distribution static blog publish system based on ipfs in Go.
-Complete documentation is unavailable yes ;(`,
+Complete documentation is unavailable yes ;`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("wecome to Papillon;)")
+	fmt.Println(" ____             _ _ _");
+	fmt.Println("|  _ \\ __ _ _ __ (_) | | ___  _ __")
+	fmt.Println("| |_) / _` | '_ \\| | | |/ _ \\| '_ \\")
+	fmt.Println("|  __/ (_| | |_) | | | | (_) | | | |")
+	fmt.Println("|_|   \\__,_| .__/|_|_|_|\\___/|_| |_|")
+        fmt.Println("|_|")
+		fmt.Println("Welcome to Papillon;")
 		fmt.Println("------------------------")
 		cmd.Help()
 		fmt.Println("------------------------")
